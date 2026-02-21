a9acb9c3f98b689c5386a27e74e70e7ac100a906
Almat <uralbaevalmat804@gmail.com>
2026-02-21 11:23:23 +0500

    ...

---
+++ 5.py
@@ -0,0 +1,69 @@
+##1
+class MyClass:
+  x = 5
+
+##2
+p1 = MyClass()
+print(p1.x)
+
+##3
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+##4
+class Person:
+  pass
+
+##5
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+p1 = Person("Emil", 36)
+print(p1.name)
+print(p1.age)
+
+##6
+class Person:
+  pass
+p1 = Person()
+p1.name = "Tobias"
+p1.age = 25
+print(p1.name)
+print(p1.age)
+
+##7
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+p1 = Person("Linus", 28)
+print(p1.name)
+print(p1.age)
+
+##8
+class Person:
+  def __init__(self, name, age=18):
+    self.name = name
+    self.age = age
+p1 = Person("Emil")
+p2 = Person("Tobias", 25)
+print(p1.name, p1.age)
+print(p2.name, p2.age)
+
+##9
+class Person:
+  def __init__(self, name, age, city, country):
+    self.name = name
+    self.age = age
+    self.city = city
+    self.country = country
+p1 = Person("Linus", 30, "Oslo", "Norway")
+print(p1.name)
+print(p1.age)
+print(p1.city)
+print(p1.country)
---
+++ 6.py
@@ -0,0 +1,40 @@
+##1
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+  def printname(self):
+    print(self.firstname, self.lastname)
+x = Person("John", "Doe")
+x.printname()
+
+##2
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+##3
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+##4
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+##5
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+x = Student("Mike", "Olsen", 2019)
+
+##6
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
